6a98cd00857a5535c8398dcaff7c1eaaff6a4ff1
Software DevelopeR <softwaredeveloperx9@gmail.com>
2026-01-22 03:25:00 +0700

    Deploy: ti.andaru-apps.co.id

--- app-frm/services/FRM_Signature.js
+++ app-frm/services/FRM_Signature.js
@@ -5,10 +5,10 @@ angular.module('app.frmUtils').factory('FRM_Signature', [
     function ($location) {
         var data = {};
 
-        data.Signature = '2026.01.22_03.22 Framework - PAS';
-        //data.Signature = '2026.01.21_19.26 Framework - TI';
-        //data.Signature = '2026.01.21_19.27 Framework - TIP';
-        //data.Signature = '2026.01.21_19.28 Framework - JLD';
+        //data.Signature = '2026.01.22_03.22 Framework - PAS';
+        data.Signature = '2026.01.22_03.24 Framework - TI';
+        //data.Signature = '2026.01.22_19.27 Framework - TIP';
+        //data.Signature = '2026.01.22_19.28 Framework - JLD';
 
         return data;
     },
