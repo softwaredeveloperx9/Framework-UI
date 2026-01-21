168cc9b0874bffbc417c3861e853bbfdb677e479
Software DevelopeR <softwaredeveloperx9@gmail.com>
2026-01-22 06:49:55 +0700

    Rename: __Library instead of __Framework

--- app-frm/services/FRM_Signature.js
+++ app-frm/services/FRM_Signature.js
@@ -5,10 +5,10 @@ angular.module('app.frmUtils').factory('FRM_Signature', [
     function ($location) {
         var data = {};
 
-        //data.Signature = '2026.01.22_03.22 Framework - PAS';
+        data.Signature = '2026.01.22_06.48 Framework - PAS';
         //data.Signature = '2026.01.22_03.24 Framework - TI';
         //data.Signature = '2026.01.22_03.27 Framework - TIP';
-        data.Signature = '2026.01.22_03.30 Framework - JLD';
+        //data.Signature = '2026.01.22_03.30 Framework - JLD';
 
         return data;
     },
