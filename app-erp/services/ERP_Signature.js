39eafff5c2ab506a529f7e1512e1ac4ccb77badf
Software DevelopeR <softwaredeveloperx9@gmail.com>
2026-01-21 19:26:31 +0700

    Deploy: ti.andaru-apps.co.id

--- app-erp/services/ERP_Signature.js
+++ app-erp/services/ERP_Signature.js
@@ -5,8 +5,8 @@ angular.module('app.erpUtils').factory('ERP_Signature', [
     function ($location) {
         var data = {};
 
-        data.Signature = '2026.01.21_19.22 Framework - PAS';
-        //data.Signature = '2026.01.21_18.29 Framework - TI';
+        //data.Signature = '2026.01.21_19.22 Framework - PAS';
+        data.Signature = '2026.01.21_19.26 Framework - TI';
         //data.Signature = '2026.01.21_17.04 Framework - TIP';
         //data.Signature = '2026.01.21_17.05 Framework - JLD';
 
