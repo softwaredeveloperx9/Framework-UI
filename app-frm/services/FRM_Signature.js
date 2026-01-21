913a4b5279159b167fb4fffbf533ae7351427de4
Software DevelopeR <softwaredeveloperx9@gmail.com>
2026-01-22 03:30:39 +0700

    Deploy: jld.andaru-apps.co.id

--- app-frm/services/FRM_Signature.js
+++ app-frm/services/FRM_Signature.js
@@ -7,8 +7,8 @@ angular.module('app.frmUtils').factory('FRM_Signature', [
 
         //data.Signature = '2026.01.22_03.22 Framework - PAS';
         //data.Signature = '2026.01.22_03.24 Framework - TI';
-        data.Signature = '2026.01.22_03.27 Framework - TIP';
-        //data.Signature = '2026.01.22_19.28 Framework - JLD';
+        //data.Signature = '2026.01.22_03.27 Framework - TIP';
+        data.Signature = '2026.01.22_03.30 Framework - JLD';
 
         return data;
     },
